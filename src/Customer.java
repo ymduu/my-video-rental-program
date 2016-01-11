f62dd7ecaabbead813a5aded7942c82a0af2cab2
Shinpei Hayashi <hayashi@se.cs.titech.ac.jp>
2016-01-11 09:58:43 +0900

    refactor: Rename Variable (Customer#amountFor, thisAmount, result)

--- src/Customer.java
+++ src/Customer.java
@@ -53,24 +53,24 @@ public class Customer {
     }
 
     private double amountFor(Rental aRental) {
-        double thisAmount = 0;
+        double result = 0;
         switch (aRental.getMovie().getPriceCode()) {
         case Movie.REGULAR:
-            thisAmount += 2;
+            result += 2;
             if (aRental.getDaysRented() > 2) {
-                thisAmount += (aRental.getDaysRented() - 2) * 1.5;
+                result += (aRental.getDaysRented() - 2) * 1.5;
             }
             break;
         case Movie.NEW_RELEASE:
-            thisAmount += aRental.getDaysRented() * 3;
+            result += aRental.getDaysRented() * 3;
             break;
         case Movie.CHILDRENS:
-            thisAmount += 1.5;
+            result += 1.5;
             if (aRental.getDaysRented() > 3) {
-                thisAmount += (aRental.getDaysRented() - 3) * 1.5;
+                result += (aRental.getDaysRented() - 3) * 1.5;
             }
             break;
         }
-        return thisAmount;
+        return result;
     }
 }

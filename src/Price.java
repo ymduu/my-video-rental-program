bdae88721da51d1f8102a25c545f0a4ca82a8059
Shinpei Hayashi <hayashi@se.cs.titech.ac.jp>
2016-01-11 10:33:18 +0900

    refactor: Move Method (Movie#getFrequentRenterPoints, Price)

--- src/Price.java
+++ src/Price.java
@@ -2,6 +2,15 @@ abstract class Price {
     abstract int getPriceCode();
 
     abstract double getCharge(int daysRented);
+
+    int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        }
+        else {
+            return 1;
+        }
+    }
 }
 
 class ChildrensPrice extends Price {

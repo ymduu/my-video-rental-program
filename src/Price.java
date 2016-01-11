901c94539d06ff8d36647863b8ee0e62ecc213a0
Shinpei Hayashi <hayashi@se.cs.titech.ac.jp>
2016-01-11 10:32:04 +0900

    refactor: Replace Conditional with Polymorphism

--- src/Price.java
+++ src/Price.java
@@ -1,32 +1,20 @@
 abstract class Price {
     abstract int getPriceCode();
 
-    double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-        case Movie.REGULAR:
-            result += 2;
-            if (daysRented > 2) {
-                result += (daysRented - 2) * 1.5;
-            }
-            break;
-        case Movie.NEW_RELEASE:
-            result += daysRented * 3;
-            break;
-        case Movie.CHILDRENS:
-            result += 1.5;
-            if (daysRented > 3) {
-                result += (daysRented - 3) * 1.5;
-            }
-            break;
-        }
-        return result;
-    }
+    abstract double getCharge(int daysRented);
 }
 
 class ChildrensPrice extends Price {
     int getPriceCode() {
         return Movie.CHILDRENS;
+    }
+
+    double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3) {
+            result += (daysRented - 3) * 1.5;
+        }
+        return result;
     }
 }
 
@@ -34,10 +22,22 @@ class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+    
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
 
 class RegularPrice extends Price {
     int getPriceCode() {
         return Movie.REGULAR;
+    }
+    
+    double getCharge(int daysRented) {
+        double result = 2;
+        if (daysRented > 2) {
+            result += (daysRented - 2) * 1.5;
+        }
+        return result;
     }
 }

dd924c7df47796897c2a81306ce3f1835328e226
ymduu <ymd2007umd@gmail.com>
2018-01-10 16:04:37 +0900

    fix extract easily

--- src/Rental.java
+++ src/Rental.java
@@ -39,11 +39,11 @@ public class Rental {
     }
 
     int getFrequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        frequentRenterPoints++;
         if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
-            return 2;
+            frequentRenterPoints++;
         }
-        else {
-            return 1;
-        }
+        return frequentRenterPoints;
     }
 }
